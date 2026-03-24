1a5809897904b87946caeb07d6a5ad13973f60c7
Samuel Mullen <samuel@samuelmullen.com>
2026-03-24 11:42:09 -0500

    SEed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+viz = User.create!(username: "viz", email: "viz@hierarchy.com")
+hyro = User.create!(username: "protagonist", email: "hyro@snowcrash.io")
+harry = User.create!(username: "harry", email: "hseldon@foundation.com")
+molly = User.create!(username: "molly", email: "mmillions@mirrorshades.com")
+
+viz.posts.create!(title: "The Will of the Lot", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+viz.posts.create!(title: "The Strength of a Couple", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+hyro.posts.create!(title: "Snow Crashed", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+harry.posts.create!(title: "A Solid Foundation", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+molly.posts.create!(title: "Mirror Shades", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+molly.posts.create!(title: "The Neuromancer", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
